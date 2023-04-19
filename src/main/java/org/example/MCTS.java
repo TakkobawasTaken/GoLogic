5b5d9a0eb8db67a23d5c311063be4c47822916d5
Jojo <josephnchuisseu@gmail.com>
2023-04-19 08:57:58 -0400

    Added the pseudo-code for the MCTS implementation

--- src/main/java/org/example/MCTS.java
+++ src/main/java/org/example/MCTS.java
@@ -1,4 +1,56 @@
 package org.example;
+import org.example.Goban;
+import org.example.Intersection;
+import org.example.Main;
+import org.example.StoneChain;
+import score.Island;
+import score.Scorer;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class MCTS {
+/*
+    private static final double EXPLORATION_CONSTANT = Math.sqrt(2);
+    private static final int NUM_SIMULATIONS = 1000;
+    private static final int BOARD_SIZE = 19;
+    private static final int EMPTY = 0;
+    private static final int BLACK = 1;
+    private static final int WHITE = 2;
+    private static final int[][] DIRECTIONS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    private static int[][] copyBoard(int[][] board) {
+        // Create a deep copy of the game board
+    }
+
+    private static boolean isValidMove(int[][] board, int player, int x, int y) {
+        // Check if a move is legal
+        // Return true if it is, false otherwise
+    }
+
+    private static int[][] makeMove(int[][] board, int player, int x, int y) {
+        // Make a move on the game board
+        // Return the new board state
+    }
+
+    private static int evaluateBoard(int[][] board) {
+        // Evaluate the current state of the board
+        // Return a score for the current player
+    }
+
+    public static int[] getBestMove(int[][] board, int player) {
+        GoNode rootNode = new GoNode(board, player, null);
+
+        for (int i = 0; i < NUM_SIMULATIONS; i++) {
+            GoNode node = rootNode;
+            int[][] state = copyBoard(board);
+
+            // Selection
+            while (!node.getUntriedMoves().isEmpty() && node.isTerminal()) {
+                node = node.selectChild();
+                state = makeMove(state, node.player, x,
+
+ */
 }
